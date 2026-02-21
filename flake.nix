f2b53e57f6bea8d44fbf6521b0bd250851cfedcf
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-02-21 14:33:11 -0400

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ 
+							bambu-studio 
+							freecad 
+							orca-slicer 
+
+							treefmt
+						];
+          };
+        };
+      });
+}
